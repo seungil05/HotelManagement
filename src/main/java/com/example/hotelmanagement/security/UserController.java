e6cdb866970eadbae95587cabafc19b345b28972
seungil05 <heusserd@bzz.ch>
2023-06-21 15:17:33 +0200

    Added Delete and Update User Methods

--- src/main/java/com/example/hotelmanagement/security/UserController.java
+++ src/main/java/com/example/hotelmanagement/security/UserController.java
@@ -3,6 +3,9 @@ package com.example.hotelmanagement.security;
 import ch.ubs.m295.generated.v1.controller.UsersApi;
 import ch.ubs.m295.generated.v1.dto.User;
 import com.example.hotelmanagement.AbstractController;
+import com.example.hotelmanagement.room.RoomController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +18,8 @@ import javax.sql.DataSource;
 
 @RestController
 public class UserController extends AbstractController implements UsersApi {
+
+    private final Logger logger = LoggerFactory.getLogger(RoomController.class);
 
     @Autowired
     private DataSource dataSource;
@@ -33,8 +38,34 @@ public class UserController extends AbstractController implements UsersApi {
                     .roles("USER")
                     .build();
             users.createUser(admin);
+            logger.info("User created: " + user.getUsername());
         }
 
         return postRespond();
+    }
+
+    @Override
+    public ResponseEntity<Void> usersPut(User user) {
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        if (users.userExists(user.getUsername()) && !user.getPassword().isEmpty()) {
+            users.deleteUser(user.getUsername());
+            users.createUser(org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUsername())
+                    .password(passwordEncoder().encode(user.getNewPassword()))
+                    .roles("USER")
+                    .build());
+            logger.info("Password changed for user: " + user.getUsername());
+        }
+        return putRespond();
+    }
+
+    @Override
+    public ResponseEntity<Void> usersUsernameDelete(String username) {
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        if (users.userExists(username)) {
+            users.deleteUser(username);
+            logger.info("User deleted: " + username);
+        }
+        return deleteRespond();
     }
 }
