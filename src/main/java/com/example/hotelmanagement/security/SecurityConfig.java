f9bd5a0075f9b440592a9a81e22bfc36da393d1d
seungil05 <heusserd@bzz.ch>
2023-06-21 13:08:20 +0200

    Changed line in security

--- src/main/java/com/example/hotelmanagement/security/SecurityConfig.java
+++ src/main/java/com/example/hotelmanagement/security/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.example.hotelmanagement.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -49,7 +49,7 @@ public class SecurityConfig {
                         .permitAll()
                 )
                 .httpBasic(Customizer.withDefaults())
-                .csrf((csrf) -> csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
